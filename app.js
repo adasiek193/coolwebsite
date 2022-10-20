080244daf42a18a5b6f95044263612b24060c37e
adasiek <90912558+adasiek193@users.noreply.github.com>
2022-10-20 16:25:56 +0200

    pyrpyr

--- app.js
+++ app.js
@@ -3,5 +3,11 @@
 		await fetch(`https://ifconfig.me/ip`)
 	).text();
 
-	document.getElementById("text").innerHTML = `${coscoaleksbardzolubi} meow ;3`;
+	document.getElementById(
+		"text"
+	).innerHTML = `${coscoaleksbardzolubi} meow ;3 `;
+
+	console.log(
+		"your ip has been sold for indo-ukrainian anons-hackers service "
+	);
 })();
